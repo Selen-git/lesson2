cd7c43089b611079045deec466db53152ba074a3
Selen-git <Selen_6966@inbox.ru>
2025-02-19 14:58:00 +0500

    Update lesson_2_task_4.py

--- 02_lesson/lesson_2_task_4.py
+++ 02_lesson/lesson_2_task_4.py
@@ -1,1 +1,9 @@
-
+fizz_buzz = input("n= ")
+n = int(fizz_buzz)
+for n in fizz_buzz :
+	if (n % 3 == "fizz"):
+		print(n)
+	if (n % 5 == "buzz"):	
+		print(n)
+	else :(n % 3 and 5 == fizz_buzz)
+print(n)
