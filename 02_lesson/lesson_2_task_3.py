44d00df9c16cef11b833ff274713c54297f6d368
Selen-git <Selen_6966@inbox.ru>
2025-02-19 16:15:58 +0500

    Update lesson_2_task_3.py

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -2,3 +2,11 @@ square = input("Сторона квадрата :")
 kvadrat = int(square)
 for x in range(0 , 1001) :
 	print("x = ", x, "x² = ", x*x)
+
+
+
+# Либо такой код, запутался не уверен какой из них правильный
+def square(side_length):  
+    area = square(side_length * 2)
+    return (area)  
+print('Площадь:') 
