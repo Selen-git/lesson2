ad161d6e6d47aae388a82cfb985b92fddf03634c
Selen-git <Selen_6966@inbox.ru>
2025-02-19 15:52:37 +0500

    Update lesson_2_task_5.py

--- 02_lesson/lesson_2_task_5.py
+++ 02_lesson/lesson_2_task_5.py
@@ -1,1 +1,14 @@
+month = int(input("Введите номер месяца: "))
+if (month < 1 or month > 12):
+    print("Ошибка! Некорректный ввод.")
+else:
+    if month in [12, 1, 2]:
+        season = "зима"
+    elif month in [3, 4, 5]:
+        season = "весна"
+    elif month in [6, 7, 8]:
+        season = "лето"
+    else:
+        season = "осень"
 
+    print(f"{month} - {season}")
