b19ee3913854363a20d4f76c2b4f0dab3ac379ca
Selen-git <Selen_6966@inbox.ru>
2025-02-22 17:45:37 +0500

    Update lesson_2_task_2.py

--- 02_lesson/lesson_2_task_2.py
+++ 02_lesson/lesson_2_task_2.py
@@ -1,7 +1,6 @@
-is_year_leap = [2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025]
+def is_year_leap(year):
+    if (year % 4 == 0 and year % 400 == 0):
+        return True
+    else:
+        return False
 
-for n in is_year_leap :
-	if (n % 4 == 0):
-		print(n)
-	else :(n % 4 == 1)
-print(n)
